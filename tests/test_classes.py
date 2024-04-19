d84add318209876fe65f20c480defb3cbdb1b0c2
Poaetce <poaetce@gmail.com>
2024-04-19 09:59:16 +0900

    added test_attributes

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -3,15 +3,27 @@ import ast
 from reference_generator.referencers import TopLevelFunctionReferencer
 
 
-def open_read_parse(file: str) -> ast.FunctionDef:
+def open_read_parse(file: str, depth: int) -> ast.FunctionDef:
     with open(file, 'r') as opened:
-        return ast.parse(opened.read()).body[0]
+        node: ast.stmt = ast.parse(opened.read())
+        for level in range(depth):
+            node = node.body[0]
+        return node
 
 
 class TestTopLevelFunctionReferencer(unittest.TestCase):
 
+    def test_attributes(self):
+        full_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/full_function.py', 1), 'functions')
+        self.assertEqual(full_function.identifier, 'hello')
+        self.assertEqual(full_function.parameters, ['name'])
+        self.assertEqual(full_function.return_type, 'str')
+        self.assertEqual(full_function.description, "greets the user")
+        self.assertEqual(full_function.reference, 'functions')
+
+
     def test_docstring_template(self):
-        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py'), 'functions')
+        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py', 1), 'functions')
         expected_base_function_docstring: str = '\n'.join([
             "<DESCRIPTION>",
             '',
@@ -19,7 +31,7 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         ])
         self.assertEqual(base_function.docstring_template(), expected_base_function_docstring)
 
-        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py'), 'functions')
+        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py', 1), 'functions')
         expected_return_function_docstring: str = '\n'.join([
             "<DESCRIPTION>",
             '',
@@ -30,7 +42,7 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         ])
         self.assertEqual(return_function.docstring_template(), expected_return_function_docstring)
 
-        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py'), 'functions')
+        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py', 1), 'functions')
         expected_parameter_function_docstring: str = '\n'.join([
             "<DESCRIPTION>",
             '',
@@ -41,7 +53,7 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         ])
         self.assertEqual(parameter_function.docstring_template(), expected_parameter_function_docstring)
 
-        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py'), 'functions')
+        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py', 1), 'functions')
         expected_multiple_parameter_function_docstring: str = '\n'.join([
             "<DESCRIPTION>",
             '',
@@ -53,7 +65,7 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         ])
         self.assertEqual(multiple_parameter_function.docstring_template(), expected_multiple_parameter_function_docstring)
 
-        full_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/full_function.py'), 'functions')
+        full_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/full_function.py', 1), 'functions')
         expected_full_function_docstring: str = '\n'.join([
             "<DESCRIPTION>",
             '',
@@ -68,14 +80,14 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         self.assertEqual(full_function.docstring_template(), expected_full_function_docstring)
 
     def test_table_item(self):
-        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py'), 'functions')
+        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py', 1), 'functions')
         expected_base_function_item: str = '\n'.join([
             "|`*hello*`",
             "|greets the user",
         ])
         self.assertEqual(base_function.table_item(), expected_base_function_item)
 
-        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py'), 'functions')
+        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py', 1), 'functions')
         expected_return_function_item: str = '\n'.join([
             "|`_str_`",
             "|`*hello*`",
@@ -84,36 +96,36 @@ class TestTopLevelFunctionReferencer(unittest.TestCase):
         self.assertEqual(return_function.table_item(), expected_return_function_item)
 
     def test_shape(self):
-        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py'), 'functions')
+        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py', 1), 'functions')
         expected_base_function_shape: str = "`functions.*hello*()`"
         self.assertEqual(base_function.shape(), expected_base_function_shape)
 
-        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py'), 'functions')
+        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py', 1), 'functions')
         expected_parameter_function_shape: str = "`functions.*hello*(_name_)`"
         self.assertEqual(parameter_function.shape(), expected_parameter_function_shape)
 
-        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py'), 'functions')
+        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py', 1), 'functions')
         expected_multiple_parameter_function_shape: str = "`functions.*hello*(_name_, _shout_)`"
         self.assertEqual(multiple_parameter_function.shape(), expected_multiple_parameter_function_shape)
 
     def test_details(self):
-        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py'), 'functions')
+        base_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/base_function.py', 1), 'functions')
         expected_base_function_details: str = f"== `hello`\n\n{base_function.shape()}\n\n{base_function.docstring}"
         self.assertEqual(base_function.details(), expected_base_function_details)
 
-        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py'), 'functions')
+        return_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/return_function.py', 1), 'functions')
         expected_return_function_details: str = f"== `hello`\n\n{return_function.shape()}\n\n{return_function.docstring}"
         self.assertEqual(return_function.details(), expected_return_function_details)
 
-        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py'), 'functions')
+        parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/parameter_function.py', 1), 'functions')
         expected_parameter_function_details: str = f"== `hello`\n\n{parameter_function.shape()}\n\n{parameter_function.docstring}"
         self.assertEqual(parameter_function.details(), expected_parameter_function_details)
 
-        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py'), 'functions')
+        multiple_parameter_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/multiple_parameter_function.py', 1), 'functions')
         expected_multiple_parameter_function_details: str = f"== `hello`\n\n{multiple_parameter_function.shape()}\n\n{multiple_parameter_function.docstring}"
         self.assertEqual(multiple_parameter_function.details(), expected_multiple_parameter_function_details)
 
-        full_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/full_function.py'), 'functions')
+        full_function: TopLevelFunctionReferencer = TopLevelFunctionReferencer(open_read_parse('tests/test_files/functions/full_function.py', 1), 'functions')
         expected_full_function_details: str = f"== `hello`\n\n{full_function.shape()}\n\n{full_function.docstring}"
         self.assertEqual(full_function.details(), expected_full_function_details)
 
